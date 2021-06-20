3d4137879d50f22b09939e37c64a7dc8ab494081
hachad-14 <hachadsaad14@gmail.com>
2021-06-20 22:11:03 +0100

    afda

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 var todaysDate = new Date();
 
-var inputDate = new Date("06/20/2021");
+var inputDate = new Date("06/22/2021");
 if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
-    document.getElementById("txt").innerHTML = "Sache que j’adore te voir heureuse\net ma plus belle récompense est\nton sourire.";
+    document.getElementById("txt").innerHTML = "Mon coeur, sache que\nje suis prêt à tout faire\npour te rendre heureuse.\nTu peux compter sur moi.";
 }
 
-var inputDate1 = new Date("06/22/2021");
+var inputDate1 = new Date("06/23/2021");
 if(inputDate1.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     document.getElementById("txt").innerHTML = "Sache que j’adore te voir heureuse\net ma plus belle récompense est\nton sourire.";
 }
 
-var inputDate2 = new Date("06/23/2021");
+var inputDate2 = new Date("06/24/2021");
 if(inputDate2.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     console.log("auj");
 }
 
-var inputDate3 = new Date("06/24/2021");
+var inputDate3 = new Date("06/25/2021");
 if(inputDate3.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     console.log("auj");
 }
 
-var inputDate4 = new Date("06/25/2021");
+var inputDate4 = new Date("06/26/2021");
 if(inputDate4.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     console.log("auj");
 }
