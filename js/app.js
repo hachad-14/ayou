f8aa9149e8726d73df73fc45612a840bdd27c757
hachad-14 <hachadsaad14@gmail.com>
2021-06-20 21:47:40 +0100

    haha

--- js/app.js
+++ js/app.js
@@ -2,25 +2,25 @@ var todaysDate = new Date();
 
 var inputDate = new Date("06/20/2021");
 if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
-    document.getElementById("txt").innerHTML = "test";
+    document.getElementById("txt").innerHTML = "teaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaast";
 }
 
-var inputDate = new Date("06/22/2021");
-if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+var inputDate1 = new Date("06/22/2021");
+if(inputDate1.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+    document.getElementById("txt").innerHTML = "pifhziaeuqfh";
+}
+
+var inputDate2 = new Date("06/23/2021");
+if(inputDate2.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     console.log("auj");
 }
 
-var inputDate = new Date("06/23/2021");
-if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+var inputDate3 = new Date("06/24/2021");
+if(inputDate3.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     console.log("auj");
 }
 
-var inputDate = new Date("06/24/2021");
-if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
-    console.log("auj");
-}
-
-var inputDate = new Date("06/25/2021");
-if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+var inputDate4 = new Date("06/25/2021");
+if(inputDate4.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
     console.log("auj");
 }
