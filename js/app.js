577ca6f79281cb32372cc4befb1bec23ba836485
hachad-14 <hachadsaad14@gmail.com>
2021-06-20 13:32:32 +0100

    fdqsfq

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var todaysDate = new Date();
+
+var inputDate = new Date("06/20/2021");
+if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+    console.log("auj");
+}
+
+var inputDate = new Date("06/21/2021");
+if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+    console.log("auj");
+}
+
+var inputDate = new Date("06/22/2021");
+if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+    console.log("auj");
+}
+
+var inputDate = new Date("06/23/2021");
+if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+    console.log("auj");
+}
+
+var inputDate = new Date("06/24/2021");
+if(inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+    console.log("auj");
+}
